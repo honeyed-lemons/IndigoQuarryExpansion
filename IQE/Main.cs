d8984c618451c00e4654debc65c9c3ec7da5e61f
Alexander Cordero <rainbowrocketplayz@outlook.com>
2021-02-22 10:07:45 -0500

    Added base stuff to main

---
+++ IQE/Main.cs
@@ -0,0 +1,27 @@
+﻿using SRML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iqe
+{
+    public class Main : ModEntryPoint
+    {
+        public override void PreLoad()
+        {
+            HarmonyInstance.PatchAll();
+        }
+
+        public override void Load()
+        {
+
+        }
+
+        public override void PostLoad()
+        {
+
+        }
+
+    }
+}
